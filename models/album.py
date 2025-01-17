0aa727c7abf6d731bc6cf3b088f9f26b084fbd0b
Kaua Pimentel <carloskkauapimentel@gmail.com>
2025-01-17 13:19:58 -0300

    enviando com m-p-m

--- models/album.py
+++ models/album.py
@@ -1,10 +1,11 @@
 from sqlmodel import SQLModel, Field, Relationship
 
 from typing import TYPE_CHECKING
+from .publicacao import Publicacao, PubAlbum
+
 
 if TYPE_CHECKING:
     from .perfil import Perfil
-    from .publicacao import Publicacao
 
 
 class AlbumBase(SQLModel):
@@ -15,4 +16,6 @@ class AlbumBase(SQLModel):
 class Album(AlbumBase, table=True):
     perfil_id: int | None = Field(default=None, foreign_key='perfil.id')
     perfil: 'Perfil' = Relationship(back_populates='albuns')
+    publicacoes: list["Publicacao"] = Relationship(back_populates="albuns",
+                                                   link_model=PubAlbum)
     
--- models/publicacao.py
+++ models/publicacao.py
@@ -6,6 +6,12 @@ if TYPE_CHECKING:
     from .perfil import Perfil
     from .album import Album
 
+class PubAlbum(SQLModel, table=True):
+    pub_id: int | None = Field(default=None, foreign_key="publicacao.id",
+                               primary_key=True)
+    album_id: int | None = Field(default=None, foreign_key="album.id",
+                                 primary_key=True) 
+
 class PublicacaoBase(SQLModel):
     id: int | None = Field(default=None, primary_key=True)
 
@@ -13,3 +19,5 @@ class Publicacao(PublicacaoBase, table=True):
     nome: str
     perfil_id: int | None = Field(default=None, foreign_key='perfil.id')
     perfil: 'Perfil' = Relationship(back_populates='publicacoes')
+    albuns: list["Album"] = Relationship(back_populates="publicacoes",
+                                         link_model=PubAlbum)
